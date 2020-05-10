9cebbf7cdf631e6f98c15826f651564cedac1b20
Lisheng <lishengyu1986@gmail.com>
2020-05-10 13:31:43 +0800

    sql works, created three tables

--- CMS-Schema.sql
+++ CMS-Schema.sql
@@ -13,7 +13,26 @@ CREATE TABLE role (
     id INT,
     title VARCHAR(30),
     salary DECIMAL(10,2),
-    department_id INT
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+        REFERENCES department (id) 
 );
 
-CREATE TABLE employee ()
+CREATE TABLE employee (
+    id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id)
+        REFERENCES role (id),
+
+    FOREIGN KEY (manager_id)
+        REFERENCES employee (id)
+);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
